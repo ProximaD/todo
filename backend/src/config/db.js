2e100e22a02bbc133983a66be37e043201d012e1
ProximaD <111272273+ProximaD@users.noreply.github.com>
2025-08-26 14:35:35 +0300

    Express App entry

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-
 async function connectDB(uri) {
-mongoose.set('strictQuery', true);
-await mongoose.connect(uri);
-console.log('✅ MongoDB connected');
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(uri);
+    console.log('MongoDB Connected');
 }
-
 
 module.exports = connectDB;
