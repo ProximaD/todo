cfa894ad7e4cc89038341b4c53b8ad364b0bcd6d
ProximaD <111272273+ProximaD@users.noreply.github.com>
2025-08-28 17:50:56 +0300

    Added auth.controller file

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,53 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const User = require('../models/User');
+const asyncHandler = require('../utils/asyncHandler');
+
+//User Registration
+exports.validateSignup = [
+    body('name').trim().notEmpty().withMessage('Name required'),
+    body('email').isEmail().withMessage('Valid email required'),
+    body('password').isLength({ min: 6 }).withMessage('Password min 6 chars')
+];
+
+exports.signup = asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return rew.status(400).jsob({ errors: errors.array() });
+
+    const { name, email, password } = req.body;
+    const exists = await User.findOne({ email });
+    if (exists) return res.status(400).json({ message: 'Email already in use '});
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, passwordHash });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+    res.status(201).json({ user: { id: user._id, name: user.name, email: user.email }, token });
+});
+
+//User Login
+exports.validateLogin = [
+    body('email').isEmail().withMessage('Valid email required'),
+    body('password').notEmpty().withMessage('Password required')
+];
+
+exports.login = asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const match = await bcrypt.compare(password, user.passwordHash);
+    if (!match) return res.status(400).json({ message: 'Invalid credentials' });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN });
+    res.json({ user: { id: user._id, name: user.name, email: user.email }, token });
+});
+
+exports.me = asyncHnadler(async (req, res) => {
+    const user = await User.findById(req.user.id).select('_id name email createdAt');
+    res.json({ user });
+});
+
+
